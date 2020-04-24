f3ab09408c2085d694699e7b0ec3c58c18359ee5
Ed Bordin <edbordin@gmail.com>
2020-04-24 15:43:34 +1000

    try building and creating circleci artifact

---
+++ run-sft.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+apt-get update && apt-get install -y --no-install-recommends git mercurial build-essential bison clang cmake \
+                     flex gawk graphviz xdot libboost-all-dev \
+                     libeigen3-dev libffi-dev libftdi-dev libgmp3-dev \
+                     libmpfr-dev libncurses5-dev libmpc-dev \
+                     libreadline-dev zlib1g-dev pkg-config python \
+                     python3 python3-dev tcl-dev autoconf gperf \
+                     qtbase5-dev libqt5opengl5-dev ca-certificates \
+                     locales automake
+
+locale-gen --no-purge en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en
+export LC_ALL=en_US.UTF-8
+
+mkdir -p ~/sft/bin
+git clone https://github.com/universal-ctags/ctags.git && cd ctags && ./autogen.sh && ./configure --prefix=~/sft && make && make install
+git clone https://github.com/thomasrussellmurphy/istyle-verilog-formatter.git && cd istyle-verilog-formatter && make && cp bin/release/iStyle ~/sft/bin
+
+git clone https://github.com/esden/summon-fpga-tools.git
+pushd summon-fpga-tools
+bash summon-fpga-tools.sh
+popd
+
+cd ~
+tar -pcvzf symbiflow-nightly.tar.gz sft
